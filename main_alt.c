a03427c32d25938d2e52d5e6a7614fd3fe4f9ff2
DuM1h <dumih00@gmail.com>
2025-06-30 20:48:00 +0300

    Initial commit

---
+++ main_alt.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int nsd(int a, int b){
+    if (b==0)
+        return a;
+    else
+        return nsd(b, a % b);
+}
+
+int nsk(int a, int b){
+	return a*b/nsd(a,b);
+}
+        
+int main(){
+	int p, i, result;
+	printf("Введіть кількість чисел: ");
+	scanf("%d", &p);
+	
+	if (p<2||p>20){
+		printf("Кількість чисел маає бути в проміжку з 2 до 20");
+		return 1;
+	}
+	
+	printf("Введіть числа: ");
+	int numbers[p];
+	for (i=0;i<p;i++){
+		scanf("%d", &numbers[i]);
+	}
+	
+	result = numbers[0];
+	for (i=1;i<p;i++){
+		result = nsk(result,numbers[i]);
+	}
+	printf("Найменше спільне кратне заданих чисел: %d", result);
+	
+	return 0;
+}
